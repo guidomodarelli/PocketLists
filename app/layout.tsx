7035cadcd470b6d8a0c8c383d2a74835adf7f7ed
gmodarelli_meli <guido.modarelli@mercadolibre.com>
2026-02-13 00:42:21 -0300

    update icon library to HugeIcons and adjust global styles
    This commit changes the icon library from Lucide to HugeIcons in the components configuration. Additionally, it updates the global CSS styles, including font changes and adjustments to various color variables for improved design consistency. The package.json and package-lock.json files have been modified to include the new HugeIcons dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 import styles from "./layout.module.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfitSans = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${styles["root-layout__body"]}`}
+        className={`${outfitSans.variable} ${geistMono.variable} ${styles["root-layout__body"]}`}
       >
         {children}
       </body>
